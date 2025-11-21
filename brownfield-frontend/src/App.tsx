fa71a26bc0d9e7e345b476618dbe55b6eb791de5
Michael Isvy <michael.isvy@gmail.com>
2025-11-22 07:04:22 +0800

    added boilerplate for brownfield projects

---
+++ brownfield-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Welcome to the Petclinic application!
+      </p>
+    </>
+  )
+}
+
+export default App
